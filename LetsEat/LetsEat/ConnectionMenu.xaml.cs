9ff72fdcd6d29c3c6c2cd4096e7f40b8f3439950
Olivier Quach <kyosukke1@gmail.com>
2016-02-12 17:46:10 +0100

    Added ConnectionMenu & MainMenu

--- LetsEat/LetsEat/ConnectionMenu.xaml.cs
+++ LetsEat/LetsEat/ConnectionMenu.xaml.cs
@@ -20,9 +20,9 @@ namespace LetsEat
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ConnectionMenu : Page
+    public sealed partial class MainPage : Page
     {
-        public ConnectionMenu()
+        public MainPage()
         {
             this.InitializeComponent();
 
@@ -61,7 +61,6 @@ namespace LetsEat
             {
                 password = this.password.Password;
             }
-            
         }
     }
 }
