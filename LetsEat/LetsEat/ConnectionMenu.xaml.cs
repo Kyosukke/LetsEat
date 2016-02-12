409f5ef910612f66a627310e0aae921321b21b7a
Alexandre Ung <alexandre.ung@azeo.com>
2016-02-12 17:24:15 +0100

    Update

---
+++ LetsEat/LetsEat/ConnectionMenu.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace LetsEat
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ConnectionMenu : Page
+    {
+        public ConnectionMenu()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private void connect_Click(object sender, RoutedEventArgs e)
+        {
+            string email = this.email.Text;
+            string password = this.password.Password;
+
+            //NavigationService.Navigate(new);
+        }
+
+        private void signIn_Click(object sender, RoutedEventArgs e)
+        {
+            string email = this.createEmail.Text;
+            string password = "";
+            if (this.password.Password == this.verifyPassword.Password)
+            {
+                password = this.password.Password;
+            }
+            
+        }
+    }
+}
